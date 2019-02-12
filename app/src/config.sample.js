8a86fb11a1150cee5f2dc9ffe42cc0582ea348a2
ishiguro-yo <ishiguro.yo@jp.fujitsu.com>
2019-02-12 18:13:28 +0900

    add dummy config data

---
+++ app/src/config.sample.js
@@ -0,0 +1,12 @@
+
+module.exports = {   
+  cellURL: 'https://demo.personium.io:443/<YOUR_CELL_NAME>/',
+  adminUser: '<YOUR_ADMIN_USERNAME>',
+  adminPass: '<YOUR_ADMIN_PASSWORD>',
+  proxy: {
+    user: '',
+    pass: ''
+  },
+  localPath: '<LOCAL_BOX_DIR>',
+  targetBox: '<REMOTE_BOX_NAME>',
+}
